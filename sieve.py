ed4566d98bf48855c681848db81c8a1ac332a919
Jammie Thai <jthai993@gmail.com>
2021-12-16 18:25:56 -0800

    feat: implement eratosthenes algorithm to solve for primes

--- sieve.py
+++ sieve.py
@@ -2,13 +2,34 @@
 def brute_primes(arr):
     # Loop through all nums in arr
     for num in arr:
+        # Only check numbers greater than 1
         if num > 1:
+            # Check if all nums divisible by nums in arr
             if all(num % i != 0 for i in range(2, num)):
+                # If not divisible
                 print(num)
 
 
-brute_primes([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-             50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
+# brute_primes([0, 1, 2, 3, 4, 5])
 
 
 # Sieve of Eratosthenes
+def sieve_of_eratosthenes(n):
+    if n <= 2:
+        return []
+    is_prime = [True] * n
+    is_prime[0] = False
+    is_prime[1] = False
+
+    # Divisor from 2
+    # up to sqrt of n
+    for i in range(2, int(n**0.5)):
+        # Mark multiples of that number as isPrime = False
+        if is_prime[i]:
+            for num in range(i*i, n, i):
+                is_prime[num] = False
+    # Use list comprehension to return arr of numbers that are prime
+    return [i for i in range(n) if is_prime[i]]
+
+
+print(sieve_of_eratosthenes(100))
